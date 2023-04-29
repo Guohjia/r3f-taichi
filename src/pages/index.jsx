8be7608f6e7b09cebcf5653b2d1c4ce43d52a2bd
guohjia <guohjia@gmail.com>
2023-04-29 22:55:00 +0800

    feat: r3f example

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,46 @@
+import { useRef, useState } from 'react';
+import queryString from 'query-string';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
 const IndexPage = () => {
+  const query = queryString.parse(window.location.search);
+  const name = query.name;
+
   return (
     <div>
-      <p>这里是首页</p>
+      <p>{`你好，${name}`}</p>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 };
